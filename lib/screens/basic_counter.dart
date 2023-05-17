6dcb4ff82ae98829c9e685f0e8d5781f1dc683c7
kodirzaelani <laman.kreasi@gmail.com>
2023-05-17 22:13:05 +0800

    first commit

---
+++ lib/screens/basic_counter.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class BasicCounter extends StatefulWidget {
+  const BasicCounter({super.key});
+
+  @override
+  State<BasicCounter> createState() => _BasicCounterState();
+}
+
+class _BasicCounterState extends State<BasicCounter> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Basic Counter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
