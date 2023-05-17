8680c2f021d2458d341e127164d512786577e33c
kodirzaelani <laman.kreasi@gmail.com>
2023-05-17 23:47:23 +0800

    Basic Stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stack/screens/basic_counter.dart';
+// import 'package:flutter_stack/screens/basic_counter.dart';
+import 'package:flutter_stack/screens/basic_stack.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const BasicCounter(),
+      home: const SampleStack(),
     );
   }
 }
---
+++ lib/screens/basic_stack.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class SampleStack extends StatelessWidget {
+  const SampleStack({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Basic Stack'),
+      ),
+      body: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned(
+            right: 0,
+            top: 0,
+            child: Container(
+              height: 230,
+              width: 230,
+              color: Colors.green,
+            ),
+          ),
+          Positioned(
+            left: 100,
+            top: 0,
+            child: Container(
+              height: 150,
+              width: 150,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
