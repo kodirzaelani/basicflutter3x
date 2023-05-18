04b2a34d5b9286ee8143665bae9e1dbe929ede9a
kodirzaelani <laman.kreasi@gmail.com>
2023-05-18 14:24:27 +0800

    Basic Dialog

--- lib/screens/basic_dialog_bottomsheet.dart
+++ lib/screens/basic_dialog_bottomsheet.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class BasicDialog extends StatelessWidget {
+class BasicDialog extends StatefulWidget {
   const BasicDialog({super.key});
 
+  @override
+  State<BasicDialog> createState() => _BasicDialogState();
+}
+
+class _BasicDialogState extends State<BasicDialog> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +21,49 @@ class BasicDialog extends StatelessWidget {
           ),
         ),
       ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: showMydialogalert,
+            child: const Text('Show Alert Dialog'),
+          ),
+          ElevatedButton(
+            onPressed: _showMyBottomSheet,
+            child: const Text('Show Bottom Sheet'),
+          ),
+        ],
+      ),
     );
+  }
+
+  void showMydialogalert() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Peringatan'),
+            content: const Text('Ini isi peringatan'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          );
+        });
+  }
+
+  void _showMyBottomSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return const SizedBox(
+            height: 200,
+            width: 300,
+            child: Text('Ini modal Bottom Sheet'),
+          );
+        });
   }
 }
